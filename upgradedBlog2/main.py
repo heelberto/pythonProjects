087cea1595045c1b9704c71485d3d6e804e5b095
unknown <gilbertgalvannn@gmail.com>
2024-10-30 21:22:38 -0700

    allows for dynamic response to the contact page for POST and GET request. Allows for form functionality, need to update to send an actual email

--- upgradedBlog2/main.py
+++ upgradedBlog2/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 FAKE_BLOG_ENDPOINT = 'https://api.npoint.io/661093b156c4cc278ba1'
@@ -6,8 +6,6 @@ FAKE_BLOG_ENDPOINT = 'https://api.npoint.io/661093b156c4cc278ba1'
 response = requests.get(url=FAKE_BLOG_ENDPOINT)
 response.raise_for_status()
 posts = response.json()
-
-
 
 app = Flask(__name__)
 
@@ -17,9 +15,18 @@ def home():
     return render_template("index.html", all_posts=posts)
 
 
-@app.route("/contact")
+@app.route("/contact", methods=['POST', 'GET'])
 def get_contact():
-    return render_template("contact.html")
+    if request.method == 'POST':
+        data = request.form
+        print(data['name'])
+        # print(f"{}")
+        # \n{request.form['email']}\n{request.form['phone']}\n{request.form['message']}")
+        return render_template("contact.html", success=1)
+    elif request.method == 'GET':
+        return render_template("contact.html")
+    else:
+        return "Error"
 
 
 @app.route("/about")
@@ -30,6 +37,8 @@ def get_about():
 @app.route("/post/<int:post_id>")
 def post_detail(post_id):
     return render_template("post.html", all_posts=posts, post_id=post_id)
+
+
 
 
 if __name__ == "__main__":
