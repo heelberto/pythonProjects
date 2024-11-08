466ab3bd3a1c190db3e5d1222da832ef4fd09c66
unknown <gilbertgalvannn@gmail.com>
2024-11-08 10:57:22 -0800

    converts submit field to the correct WTForms syntax, as well as creates better formatting for the log in form field

--- day-61-starting-files-flask-secrets/main.py
+++ day-61-starting-files-flask-secrets/main.py
@@ -1,25 +1,13 @@
 import flask
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     email = StringField(label='Email', validators=[DataRequired()])
     password = PasswordField(label='Password', validators=[DataRequired()])
-
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
+    submit = SubmitField(label='Log in')
 
 
 app = Flask(__name__)
