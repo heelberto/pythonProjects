5eb476bb15da0ad019f099d2876af1389a2e787c
unknown <gilbertgalvannn@gmail.com>
2024-11-11 12:20:41 -0800

    adds template inheritance w/ Jinja2, adds base.html, adjusts success/denied.html to be build upon base.html

--- day-61-starting-files-flask-secrets/main.py
+++ day-61-starting-files-flask-secrets/main.py
@@ -2,11 +2,11 @@ import flask
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, Length
 
 class LoginForm(FlaskForm):
-    email = StringField(label='Email', validators=[DataRequired()])
-    password = PasswordField(label='Password', validators=[DataRequired()])
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
     submit = SubmitField(label='Log in')
 
 
@@ -18,16 +18,23 @@ app.secret_key = 'secret'
 def home():
     return render_template('index.html')
 
+
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        return flask.redirect('/success')
+        if form.email.data == "admin@email.com" and form.password.data == "12345678":
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
+         #print(f"{form.email.data}\n{form.password.data}")
     return render_template('login.html', form=form)
+
 
 @app.route("/success")
 def success():
     return render_template('success.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
