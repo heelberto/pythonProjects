554180a308dfaac09d1f43876435a3856337b2af
unknown <gilbertgalvannn@gmail.com>
2024-12-08 19:40:23 -0800

    creates the functionality of displaying of all current movies inside of the database

--- day-64-starting-files-top-movies/main.py
+++ day-64-starting-files-top-movies/main.py
@@ -45,10 +45,27 @@ class Movie(db.Model):
 with app.app_context():
     db.create_all()
 
+"""
+# Create and add a new movie
+with app.app_context():
+    new_movie = Movie(
+        title="Avatar The Way of Water",
+        year=2022,
+        description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+        rating=7.3,
+        ranking=9,
+        review="I liked the water.",
+        img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
+    )
+    db.session.add(new_movie)
+    db.session.commit()
+"""
+
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    all_movies = Movie.query.all()
+    return render_template("index.html", movies=all_movies)
 
 
 if __name__ == '__main__':
