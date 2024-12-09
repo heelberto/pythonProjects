74f6680fbda3c1c1de14bede09b3b466e39ae903
unknown <gilbertgalvannn@gmail.com>
2024-12-08 18:36:42 -0800

    creates code to set up initial empty database, seems to be successful, will attempt to add to the database now

--- day-64-starting-files-top-movies/main.py
+++ day-64-starting-files-top-movies/main.py
@@ -8,27 +8,42 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
 
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
 
+
 # CREATE DB
+class Base(DeclarativeBase):
+    pass
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movie-collection.db"
+
+
+# CREATE THE EXTENSTION
+db = SQLAlchemy(model_class=Base)
+
+#INITIALISE THE APP WITH THE EXTENSION
+db.init_app(app)
 
 
 # CREATE TABLE
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
+    description: Mapped[str] = mapped_column(String(500), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+    ranking: Mapped[int] = mapped_column(Integer, nullable=False)
+    review: Mapped[str] = mapped_column(String(500), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
+
+# Create table schema in the database, requires application context
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
