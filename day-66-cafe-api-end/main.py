f9d1e9d93dde97e055009fd526358d8dcc117d32
unknown <gilbertgalvannn@gmail.com>
2025-03-11 00:23:40 -0700

    adds day-66

--- day-66-cafe-api-end/main.py
+++ day-66-cafe-api-end/main.py
@@ -3,19 +3,31 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Boolean
 import random
-app = Flask(__name__)
 
+'''
+Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+'''
+
+app = Flask(__name__)
 
 # CREATE DB
 class Base(DeclarativeBase):
     pass
-# Connect to Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
 
-# Cafe TABLE Configuration
+# CREATE TABLE
 class Cafe(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
@@ -30,7 +42,6 @@ class Cafe(db.Model):
     coffee_price: Mapped[str] = mapped_column(String(250), nullable=True)
 
     def to_dict(self):
-        dictionary = {}
         return {column.name: getattr(self, column.name) for column in self.__table__.columns}
 
 
@@ -43,57 +54,38 @@ def home():
     return render_template("index.html")
 
 
-# HTTP GET - Read Record - return all current cafes in db
 @app.route("/random")
-def random_cafe():
-    cafes = db.session.execute(db.select(Cafe)).scalars()
-    cafes_list = list(cafes)
-    rand_cafe = random.choice(cafes_list)
+def get_random_cafe():
+    result = db.session.execute(db.select(Cafe))
+    all_cafes = result.scalars().all()
+    random_cafe = random.choice(all_cafes)
+    return jsonify(cafe=random_cafe.to_dict())
 
-    def cafe_to_dict(cafe):
-        return {
-            "id": cafe.id,
-            "name": cafe.name,
-            "map_url": cafe.map_url,
-            "img_url": cafe.img_url,
-            "location": cafe.location,
-            "seats": cafe.seats,
-            "has_toilet": cafe.has_toilet,
-            "has_wifi": cafe.has_wifi,
-            "has_sockets": cafe.has_sockets,
-            "can_take_calls": cafe.can_take_calls,
-            "coffee_price": cafe.coffee_price,
-        }
-    return jsonify(cafe_to_dict(rand_cafe))
 
-    # alternate way of passing each 'column' from a cafe individually
-    # return jsonify(name=rand_cafe.name)
-
-# return all cafes
 @app.route("/all")
-def get_all():
-    cafes = db.session.execute(db.select(Cafe)).scalars().all()
-    return jsonify(cafes=[cafe.to_dict() for cafe in cafes])
+def get_all_cafes():
+    result = db.session.execute(db.select(Cafe).order_by(Cafe.name))
+    all_cafes = result.scalars().all()
+    return jsonify(cafes=[cafe.to_dict() for cafe in all_cafes])
 
 
-# Search for a cafe by city name
 @app.route("/search")
-def cafe_search():
+def get_cafe_at_location():
     query_location = request.args.get("loc")
     result = db.session.execute(db.select(Cafe).where(Cafe.location == query_location))
+    # Note, this may get more than one cafe per location
     all_cafes = result.scalars().all()
     if all_cafes:
         return jsonify(cafes=[cafe.to_dict() for cafe in all_cafes])
     else:
         return jsonify(error={"Not Found": "Sorry, we don't have a cafe at that location."}), 404
 
-
-# HTTP POST - Create Record
+# Test this inside Postman. Request type: Post ->  Body ->  x-www-form-urlencoded
 @app.route("/add", methods=["POST"])
 def post_new_cafe():
     new_cafe = Cafe(
         name=request.form.get("name"),
-        map_url=request.form.get("map_url") or request.args.get("map_url"),
+        map_url=request.form.get("map_url"),
         img_url=request.form.get("img_url"),
         location=request.form.get("loc"),
         has_sockets=bool(request.form.get("sockets")),
@@ -105,31 +97,35 @@ def post_new_cafe():
     )
     db.session.add(new_cafe)
     db.session.commit()
-    return jsonify(response={'success': 'Successfully added the new cafe.'}), 200
+    return jsonify(response={"success": "Successfully added the new cafe."})
 
-# HTTP PUT/PATCH - Update Record
-
-
-@app.route("/update-price/<cafe_id>", methods=["PATCH"])
-def update_price(cafe_id):
-    if db.get_or_404(Cafe, cafe_id):
-        cafe_to_edit = db.get_or_404(Cafe, cafe_id)
-        cafe_to_edit.coffee_price = request.form.get('new_price')
+# Updating the price of a cafe based on a particular id:
+# http://127.0.0.1:5000/update-price/CAFE_ID?new_price=£5.67
+@app.route("/update-price/<int:cafe_id>", methods=["PATCH"])
+def patch_new_price(cafe_id):
+    new_price = request.args.get("new_price")
+    cafe = db.get_or_404(Cafe, cafe_id)
+    if cafe:
+        cafe.coffee_price = new_price
         db.session.commit()
-        return jsonify(response={'success': 'Successfully updated the price.'})
+        return jsonify(response={"success": "Successfully updated the price."}), 200
     else:
-        return jsonify(error={'error': 'Sorry a cafe with that id was not found in the database'})
+        return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."}), 404
 
-@app.route("/report-closed/<cafe_id>", methods=["DELETE"])
-def report_closed(cafe_id):
-    if db.get_or_404(Cafe, cafe_id):
-        cafe_to_delete = db.get_or_404(Cafe, cafe_id)
-        db.session.delete(cafe_to_delete)
-        db.session.commit()
-        return  jsonify(response={'success': 'Successfully removed the cafe from the list.'})
-
-
-# HTTP DELETE - Delete Record
+# Deletes a cafe with a particular id. Change the request type to "Delete" in Postman
+@app.route("/report-closed/<int:cafe_id>", methods=["DELETE"])
+def delete_cafe(cafe_id):
+    api_key = request.args.get("api-key")
+    if api_key == "TopSecretAPIKey":
+        cafe = db.get_or_404(Cafe, cafe_id)
+        if cafe:
+            db.session.delete(cafe)
+            db.session.commit()
+            return jsonify(response={"success": "Successfully deleted the cafe from the database."}), 200
+        else:
+            return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."}), 404
+    else:
+        return jsonify(error={"Forbidden": "Sorry, that's not allowed. Make sure you have the correct api_key."}), 403
 
 
 if __name__ == '__main__':
