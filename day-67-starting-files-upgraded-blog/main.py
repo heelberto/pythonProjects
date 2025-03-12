7ee320c152f2f0a59de323feb143c5573dc80768
unknown <gilbertgalvannn@gmail.com>
2025-03-11 23:28:36 -0700

    adds functionality of links to each individual posts. retrieved from the database

--- day-67-starting-files-upgraded-blog/main.py
+++ day-67-starting-files-upgraded-blog/main.py
@@ -40,15 +40,15 @@ with app.app_context():
 
 @app.route('/')
 def get_all_posts():
-    # TODO: Query the database for all the posts. Convert the data to a python list.
     posts = db.session.execute(db.select(BlogPost)).scalars()
     return render_template("index.html", all_posts=posts)
 
+
 # TODO: Add a route so that you can click on individual posts.
-@app.route('/')
+@app.route('/show_post/<post_id>')
 def show_post(post_id):
     # TODO: Retrieve a BlogPost from the database based on the post_id
-    requested_post = "Grab the post from your database"
+    requested_post = db.get_or_404(BlogPost, post_id)
     return render_template("post.html", post=requested_post)
 
 
